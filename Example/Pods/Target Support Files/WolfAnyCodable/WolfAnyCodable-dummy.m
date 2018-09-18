cfa54598b7fbf0cc0d8828861228b16d039c56be
Wolf McNally <wolf@wolfmcnally.com>
2018-09-18 16:54:56 -0700

    Removed Pods directory.

--- Example/Pods/Target Support Files/WolfAnyCodable/WolfAnyCodable-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_WolfAnyCodable : NSObject
-@end
-@implementation PodsDummy_WolfAnyCodable
-@end
---
+++ WolfGraph/Classes/WolfGraph/Tree.swift
@@ -0,0 +1,8 @@
+//
+//  Tree.swift
+//  WolfGraph
+//
+//  Created by Wolf McNally on 9/14/18.
+//
+
+import Foundation
